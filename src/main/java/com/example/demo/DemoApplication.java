02a4e61680d109b6e5faa4ffe1ae75611213ce85
gfmolon <gfmolon@gmail.com>
2025-11-28 20:00:57 -0300

    Rodando projeto pelo terminal

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,6 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
